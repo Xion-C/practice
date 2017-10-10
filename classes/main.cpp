b8f8028b74134fb686003d1f70b4795e06a0032b
xcheng2-cslab <xcheng2@ada16.cs.clemson.edu>
2017-10-10 16:15:32 -0400

    class

--- classes/main.cpp
+++ classes/main.cpp
@@ -7,46 +7,62 @@ public:
 	string() : 
 	buf(new char[1]) {
 		buf[0] = '\0';
-		std::cout << "default" << strlen(buf) <<std::endl;
+		std::cout << "default " << strlen(buf) <<std::endl;
 	}
 	string(const char* s) : 
 	buf(new char[strlen(s)+1]) {
 		//buf[0] = '\0'; //seems no need
 		strcpy(buf, s);
 		std::cout << "conversion" <<std::endl;
-		if (buf[6]=='\0') std::cout << "yes" <<std::endl;
+		if (buf[6]=='\0') std::cout << "end \\0 yes" <<std::endl;
 		else std::cout << "no" <<std::endl;
 	}
+    //explicit string(const string & s) : 
 	string(const string & s) : 
 	buf(new char(strlen(s.buf)+1)) {
 		strcpy(buf, s.buf);
 		std::cout << "copy" <<std::endl;
 	}
-
 	~string() {
 		std::cout << "delete : " << buf <<std::endl;
 		delete [] buf;
 	}
+    string& operator=(const string & rhs) {
+        if(this == &rhs) return *this;
+        delete [] buf;
+        buf = new char[strlen(rhs.buf)+1];
+        strcpy(buf, rhs.buf);
+        return *this;
+    }
+    int aaa(int a) const { a=2;int b;b=5;return b;}
+    int bbb(int c) const { c=2;c=aaa(10);iii=3;return c;}
 
 private:
 	char *buf;
+    const int iii=11;
 };
-
-int tryconst(int m)
-{
-	return m+1;
-}
 
 int main() {
 
-	string a;
+    std::cout << "----------" << std::endl;
+	//string a;
+    std::cout << "----------" << std::endl;
 	string b("abcde");
-	std::cout << "string " << "" << std::endl;
-	//std::cout << "char " << b << std::endl;
-	int m1=10;
-	const int m2=20;
-	tryconst(m1);
-	tryconst(m2);
+    std::cout << "----------" << std::endl;
+    //string c(b);
+    std::cout << "----------" << std::endl;
+    //string d;
+    std::cout << "----------" << std::endl;
+    string e;
+    //e = b;
+    std::cout << "----------" << e.aaa(1) <<std::endl;
+    e.bbb(2);
+
+
+	//std::cout << "string " << a << std::endl;
+	//std::cout << "string " << b << std::endl;
+	//std::cout << "string " << c << std::endl;
+	//std::cout << "string " << d << std::endl;
 
 	return 0;
 } 
