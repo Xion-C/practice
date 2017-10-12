08a04f00d72fe2e08e933d5c21da88186ef2d773
xcheng2-cslab <xcheng2@ada16.cs.clemson.edu>
2017-10-12 10:52:09 -0400

    827

--- 827/asgn3/parser/test.py
+++ 827/asgn3/parser/test.py
@@ -11,6 +11,7 @@ def testCode( retcode, msg ):
     sys.exit( 1 )
 
 testDir = os.path.join( os.getcwd(), 'cases')
+outDir = os.path.join(os.getcwd(), "tmp/out")
 if not os.path.isdir( testDir ):
   print testDir, "isn't a directory"
   sys.exit( 1 )
@@ -23,11 +24,11 @@ retcode = subprocess.call("make",shell=True)
 testCode( retcode, "\tFAILED to make the scanner" )
 
 files = os.listdir( testDir )
-outdir = "./tme/out"
+print('Output Directory : ' + outDir)
 for x in files:
   if fnmatch.fnmatch(x, "*.py"):
     testcase = os.path.join(testDir, x)
-    retcode = subprocess.call("./run < "+testcase+"> "+outdir,shell=True)
+    retcode = subprocess.call("./run < "+testcase+"> "+outDir,shell=True)
     if retcode != 0:
       testCode( retcode, "\tFAILED to run test case "+x)
     else:
@@ -35,7 +36,7 @@ for x in files:
       if not os.path.isfile( output ):
         print "test case", x[:-3]+'.out', "doesn't exist"
         sys.exit( 1 )
-      if not filecmp.cmp(outdir, output): 
+      if not filecmp.cmp(outDir, output): 
         print "\tTEST CASE FAILED", x
       else :
         print "testcase:", x, "passed"
