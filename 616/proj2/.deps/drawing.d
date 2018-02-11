f13c24cd5c7832ccde73e35732ac8c948577c511
xcheng2-cslab <xcheng2@ada17.cs.clemson.edu>
2018-02-10 23:05:14 -0500

    updated

---
+++ 616/proj2/.deps/drawing.d
@@ -0,0 +1,5 @@
+drawing.o: drawing.cpp drawing.h vector2f.h
+
+drawing.h:
+
+vector2f.h:
---
+++ 616/proj2/.deps/main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp frameGenerator.h drawing.h vector2f.h
+
+frameGenerator.h:
+
+drawing.h:
+
+vector2f.h:
