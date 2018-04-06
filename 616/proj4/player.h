188e3ef21eba74dab0d4f57b8be05b1e45533c50
Xin Cheng <emchengxin@outlook.com>
2018-04-06 00:38:35 -0400

    player

---
+++ 616/proj4/player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER__H
+#define PLAYER__H
+
+#include <string>
+#include <vector>
+#include <cmath>
+#include "multisprite.h"
+
+Class Player {
+public:
+    Player();
+private:
+    Multisprite sprite;
+}
+
+#endif PLAYER__H
